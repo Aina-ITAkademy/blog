4c8fed5c6e715174de56a88031ab42c58bf3d989
Nary Aina <aa.naryandriambelo@it-students.fr>
2019-03-08 16:14:34 +0100

    Correction de nom/chemin css

--- articles.php
+++ articles.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Page des Articles</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/un_article.css" />
     <link rel="stylesheet" type="text/css" media="screen" href="css/articles_main.css" />
     <script src="js/main_article.js"></script>
 </head>
